4abad75bef47b3ab784e6054044b50c6b3109bdb
Marcel Hans <marcelsasongko11@gmail.com>
2023-08-09 14:13:29 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---Data Mahasiswa---");
+        System.out.println("Nama : Marcel Hans Sasongko");
+        System.out.println("NRP : c14230099");
+        System.out.println("Mata Kuliah : Algoritma dan Pemrogaman");
+        System.out.println("Kelas : B");
+        System.out.println("Tahun : 2023/2024");
+        System.out.println("Semester : Gasal");
+        System.out.println("---------------------");
+    }
+}
